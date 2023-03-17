1e6e12584bb01e83dfd351e84fdf3178574dd0d7
Suhas A <113137561+suhasamaresh@users.noreply.github.com>
2023-03-17 23:58:49 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "pathform() err:%v", err)
+	}
+
+	fmt.Fprintf(w, "POST request sucessfull")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "name = %s\n", name)
+	fmt.Fprintf(w, "address = %s\n", address)
+	return
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Command not found", http.StatusNotFound)
+	}
+	fmt.Printf("Hello")
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+
+	fmt.Printf("Server started at port 8080\n")
+	if err := http.ListenAndServe("8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
